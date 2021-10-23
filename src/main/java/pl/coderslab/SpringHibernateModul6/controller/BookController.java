c78a7a3495c48f7d0601a6063e68e7f916c48389
bbobrek <bartosz.bobrek1993@gmail.com>
2021-10-23 15:01:30 +0200

    relacje

--- src/main/java/pl/coderslab/SpringHibernateModul6/controller/BookController.java
+++ src/main/java/pl/coderslab/SpringHibernateModul6/controller/BookController.java
@@ -5,25 +5,46 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.coderslab.SpringHibernateModul6.dao.BookDao;
+import pl.coderslab.SpringHibernateModul6.dao.PublisherDao;
 import pl.coderslab.SpringHibernateModul6.entity.Book;
+import pl.coderslab.SpringHibernateModul6.entity.Publisher;
 
 @Controller
 @RequestMapping("/book")
 public class BookController {
 
     private final BookDao bookDao;
+    private final PublisherDao publisherDao;
 
-    public BookController(BookDao bookDao) {
+    public BookController(BookDao bookDao, PublisherDao publisherDao) {
         this.bookDao = bookDao;
+        this.publisherDao = publisherDao;
+    }
+
+    @RequestMapping("/saveWithExistingPublisher")
+    @ResponseBody
+    public String saveWithExistingPublisher() {
+        Publisher publisher = publisherDao.findById(2);
+        Book book = new Book();
+        book.setTitle("W pustyni i w puszczy");
+        book.setDescription("Ksiązka o pustyni i puszczy");
+        book.setRating(4);
+        book.setPublisher(publisher);
+        bookDao.persist(book);
+        return book.toString();
     }
 
     @RequestMapping("/save")
     @ResponseBody
     public String save() {
+        Publisher publisher = new Publisher();
+        publisher.setName("PWN");
+        publisherDao.persist(publisher);
         Book book = new Book();
         book.setTitle("W pustyni i w puszczy");
         book.setDescription("Ksiązka o pustyni i puszczy");
         book.setRating(4);
+        book.setPublisher(publisher);
         bookDao.persist(book);
         return book.toString();
     }
